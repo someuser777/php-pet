e0e48213ee3033de269f5a7e58784569b883790b
Maikovskii Daniil <maikovsky.danil@gmail.com>
2026-03-12 12:52:58 +0300

    add create_post.php api, and form "Create post" (ugly, but it is MVP)

---
+++ create_post.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['submit']) && $_POST['content']) {
+  $data_old = file_get_contents('storage/posts.json');
+  $data_old_json = json_decode($data_old);
+
+  $new_item = [
+    'id' => time(),
+    'title' => $_POST['title'] ?? '',
+    'content' => $_POST['content'],
+    'created' => date('Y-m-d H:i:s'),
+    'updated' => null,
+  ];
+
+  $data_new_json = [$new_item, ...$data_old_json];
+  $data_new = json_encode($data_new_json);
+
+  file_put_contents('storage/posts.json', $data_new);
+}
+
+header('Location: index.php');
+?>
--- index.php
+++ index.php
@@ -5,8 +5,23 @@ require('header.php');
 require_once('render_posts.php');
 ?>
 <h1>Welcome Page</h1>
+<h2>Create new post:</h1>
+<form name="create_post" method="post" action="create_post.php">
+  <label>title:</label>
+  <br />
+  <input type="text" name="title" placeholder="title" />
+  <br />
+  <label>content:</label>
+  <br />
+  <input type="text" name="content" placeholder="content" />
+  <br />
+  <br />
+  <button name="submit" value="submit">
+    Submit
+  </button>
+</form>
 <div id="posts">
-    <?=render_posts()?>
+  <?=render_posts()?>
 </div>
 <?php
 require('footer.php');
